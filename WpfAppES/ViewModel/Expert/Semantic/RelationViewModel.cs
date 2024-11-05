5647a8b04b34dacf9489ae6d88dcbb6db4d88600
Михаил Созонтов <sozmi@mail.ru>
2024-11-05 17:27:31 +0300

    Изменение типа интерфейса

---
+++ WpfAppES/ViewModel/Expert/Semantic/RelationViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfAppES.ViewModel.Expert
+{
+    public class RelationViewModel(string label)
+    {
+        public string Label { get; set; } = label;
+    }
+}
