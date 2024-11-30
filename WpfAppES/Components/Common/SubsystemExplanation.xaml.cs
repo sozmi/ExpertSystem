398d9ce125eb0e6701e7c534fd2a3d94b81381c4
Михаил Созонтов <sozmi@mail.ru>
2024-11-30 17:24:09 +0300

    добавление заглушки подсистемы объяснений

---
+++ WpfAppES/Components/Common/SubsystemExplanation.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfAppES.Components.Common
+{
+    /// <summary>
+    /// Логика взаимодействия для SubsystemExplanation.xaml
+    /// </summary>
+    public partial class SubsystemExplanation : UserControl
+    {
+        public SubsystemExplanation()
+        {
+            InitializeComponent();
+        }
+    }
+}
