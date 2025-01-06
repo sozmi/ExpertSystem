a01ef0127290d9d3b65a1c61cc844aeee959e52a
Danila25 <zaikin.danila.03@mail.ru>
2025-01-06 19:34:52 +0300

    XAML 1)Установка Content для Expander CS-файл 1)Исправление метода добавления вопросов и ответов

--- WpfAppES/Components/Common/SubsystemExplanationViewModel.cs
+++ WpfAppES/Components/Common/SubsystemExplanationViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.ObjectModel;
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Runtime.CompilerServices;
 using WpfAppES.ViewModel.BaseObjects;
@@ -7,9 +8,9 @@ namespace WpfAppES.Components.Common
 {
     public class SubsystemExplanationViewModel : BaseViewModel
     {
-        private ObservableCollection<string> _questionsAndAnswers = new ObservableCollection<string>();
+        private ObservableCollection<QuestionAnswerPair> _questionsAndAnswers = new ObservableCollection<QuestionAnswerPair>();
 
-        public ObservableCollection<string> QuestionsAndAnswers
+        public ObservableCollection<QuestionAnswerPair> QuestionsAndAnswers
         {
             get => _questionsAndAnswers;
             set => SetProperty(ref _questionsAndAnswers, value);
@@ -18,21 +19,23 @@ namespace WpfAppES.Components.Common
         public SubsystemExplanationViewModel()
         {
             // Инициализация списка QuestionsAndAnswers тестовыми данными
-            QuestionsAndAnswers = new ObservableCollection<string>
+            QuestionsAndAnswers = new ObservableCollection<QuestionAnswerPair>
             {
-                "Это",
-                "Тестовые",
-                "Данные",
-                "Для",
-                "Списка"
+                new QuestionAnswerPair { Question = "Какой город является столицей Франции?", Answer = "Париж." },
+                new QuestionAnswerPair { Question = "Как называется самая высокая гора в мире?", Answer = "Эверест." },
+                new QuestionAnswerPair { Question = "Кто написал роман 'Война и мир'?", Answer = "Лев Толстой." }
             };
         }
 
-        private void AddQuestionAnswer(string systemMessage, string userAction, string explanation)
+        private void AddQuestionAnswer(string question, string answer)
         {
-            QuestionsAndAnswers.Add(systemMessage);   // Добавляем элемент без вызова OnPropertyChanged
-            QuestionsAndAnswers.Add(userAction);      // Добавляем элемент без вызова OnPropertyChanged
-            QuestionsAndAnswers.Add(explanation);     // Добавляем элемент без вызова OnPropertyChanged
+            QuestionsAndAnswers.Add(new QuestionAnswerPair { Question = question, Answer = answer });
         }
+    }
+
+    public class QuestionAnswerPair
+    {
+        public string? Question { get; set; }
+        public string? Answer { get; set; }
     }
 }
