7b8236da0e3e4277c9c12115af4a31af3c5f6fdf
Михаил Созонтов <sozmi@mail.ru>
2025-01-12 22:11:57 +0300

    исправление форматирования

--- ClassLibraryES/products_es/Rule.cs
+++ ClassLibraryES/products_es/Rule.cs
@@ -1,13 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
-namespace ClassLibraryES.products_es
+namespace ClassLibraryES.products_es;
+
+public class Rule
 {
-    public class Rule
-    {
-        public Guid Id { get; private set; }
-        public string? Name { get; set; }
-        public List<Fact> Premises { get; private set; }
-        public Fact? Result { get; set; }
+    public Guid Id { get; private set; }
+    public string? Name { get; set; }
+    public List<Fact> Premises { get; private set; }
+    public Fact? Result { get; set; }
 
     public Rule()
     {
@@ -15,36 +15,35 @@ namespace ClassLibraryES.products_es
         Premises = new();
     }
 
-        public void AddPremise(Fact premise)
+    public void AddPremise(Fact premise)
+    {
+        if (!Premises.Contains(premise))
         {
-            if (!Premises.Contains(premise))
-            {
-                Premises.Add(premise);
-            }
+            Premises.Add(premise);
         }
+    }
 
-        /// <summary>
-        /// Обновляет свойства правила
-        /// </summary>
-        public void Update(string? name, Fact? result, IEnumerable<Fact> premises)
+    /// <summary>
+    /// Обновляет свойства правила
+    /// </summary>
+    public void Update(string? name, Fact? result, IEnumerable<Fact> premises)
+    {
+        Name = name;
+        Result = result;
+        Premises.Clear();
+        foreach (var premise in premises)
         {
-            Name = name;
-            Result = result;
-            Premises.Clear();
-            foreach (var premise in premises)
-            {
-                AddPremise(premise);
-            }
+            AddPremise(premise);
         }
+    }
 
-        public bool CheckPremises(ICollection<Fact> facts)
+    public bool CheckPremises(ICollection<Fact> facts)
+    {
+        foreach (var premise in Premises)
         {
-            foreach (var premise in Premises)
-            {
-                if (!facts.Contains(premise))
-                    return false;
-            }
-            return true;
+            if (!facts.Contains(premise))
+                return false;
         }
+        return true;
     }
 }
